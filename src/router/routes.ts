a50655cca7e3ad74d081d3850a26324d36b348b7
Anderson Henrique <dev24@redepos.com.br>
2023-09-08 16:25:47 -0300

    router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,7 +3,9 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    children: [{ path: '', component: () => import('pages/charts/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/charts/IndexPage.vue') }, {
+      path: 'page', name: 'page', component: () => import('pages/charts/IndexPage.vue')
+    }],
   },
 
   // Always leave this as last one,
